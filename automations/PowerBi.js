d85a30ea765a0cff1ee2f7e7b41ce26ab671bcb4
barani0709 <baranidharan@elbrit.org>
2025-05-19 18:00:16 +0530

    refresh update

--- automations/PowerBi.js
+++ automations/PowerBi.js
@@ -4,7 +4,7 @@ const EMAIL = 'integrations@elbrit.org';       // 🔐 Replace with your actual 
 const PASSWORD = 'F^983194242330ac12A';       // 🔐 Replace with your actual password
 
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
